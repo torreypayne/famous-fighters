a369e5fa96865a875dbea194821876e9bf20f666
Torrey Payne <torreyl.payne@gmail.com>
2015-06-26 02:43:11 -0700

    Implement some basics; need to implement Physics Engine

--- src/js/GameApp.js
+++ src/js/GameApp.js
@@ -1,19 +1,32 @@
+// var famous = require('famous');
 var PointLight = require('famous/webgl-renderables/lights/PointLight');
-var FamousEngine = require('famous/core/FamousEngine');
+// var Engine     = require('famous/core/Engine');
+
 var Mesh = require('famous/webgl-renderables/Mesh');
 var Camera = require('famous/components/Camera');
 var Color = require('famous/utilities/Color');
 var DOMElement = require('famous/dom-renderables/DOMElement');
-var PhysicsEngine = require('famous/physics/PhysicsEngine');
-var world = new PhysicsEngine();
+var math = require('famous/math');
+var physics = require('famous/physics');
+// var PhysicsEngine = require('famous/physics/PhysicsEngine');
 // var Force = PhysicsEngine.Force;
 // var DeviceView = require('./DeviceView');
 
 function GameApp(scene, num) {
+  var Gravity1D = physics.Gravity1D;
+  var Gravity3D = physics.Gravity3D;
+  var Particle = physics.Particle;
+  var Sphere = physics.Sphere;
+  var world = new physics.PhysicsEngine();
   var camera = new Camera(scene).setDepth(1000);
+
+  var FamousEngine = require('famous/core/FamousEngine');
+  var mainContext = FamousEngine.createContext();
 
   var numAsteroids = num || 5;
   var asteroidNodes = [];
+  var asteroidBodies = [];
+  var asteroidViews = [];
 
   var gameNode = scene.addChild()
       .setOrigin(0.5, 0.5, 0.5)
@@ -39,14 +52,32 @@ function GameApp(scene, num) {
     asteroidNodes.push(asteroidNode);
   }
 
+  var shipSphere = new Sphere({ mass: 50, radius: 50 }).setVelocity([1,1,0]);
+  // var asteroidSphere = new Sphere({ mass: 100, radius: 25 });
+
 
   // var gameCanvas = new Mesh(gameNode).setGeometry('Box');
 
-  var ship = new Mesh(shipNode).setGeometry('Sphere');
+  var ship = new Mesh(shipNode, shipSphere).setGeometry('Sphere', { detail: 100 });
   for (var i = 0; i < numAsteroids; i++) {
-    var asteroid = new Mesh(asteroidNodes[i]).setGeometry('Sphere');
+    var asteroidSphere = new Sphere({ mass: 100, radius: 25 });
+    asteroidBodies.push(asteroidSphere);
+    var asteroid = new Mesh(asteroidNodes[i], asteroidSphere).setGeometry('Sphere');
+    asteroidViews.push(asteroid);
+    // var asteroid = new Mesh(asteroidNodes[i]).setGeometry('Sphere');
   }
 
+  for (var i = 0; i < asteroidBodies.length; i++) {
+    var gravity = new Gravity3D(asteroidBodies[i], asteroidBodies, {strength: 300});
+    world.addForce(gravity);
+  }
+
+
+  world.add([shipSphere].concat(asteroidBodies));
+
+  var Collision = physics.Collision;
+  var collision = new Collision([shipSphere].concat(asteroidBodies));
+  world.addConstraint(collision);
 
   var lightNode = scene.addChild().setAlign(0.5, 0.5, 0.5).setPosition(0, 0, 250);
 
@@ -60,8 +91,11 @@ function GameApp(scene, num) {
   //   initialPush.add(asteroidNodes[i]);
   // }
 
+  mainContext.add(asteroidViews[0]);
+
   FamousEngine.getClock().setInterval(function() {
     var time = clock.getTime();
+    world.update(time);
 
     shipNode.setRotation(
       time / 1500,
