519a90cf375d971a4289485fe80318471df8c69f
Torrey Payne <torreyl.payne@gmail.com>
2015-06-26 20:18:28 -0700

    Still working on Physics

--- src/js/GameApp.js
+++ src/js/GameApp.js
@@ -1,27 +1,22 @@
-// var famous = require('famous');
 var PointLight = require('famous/webgl-renderables/lights/PointLight');
-// var Engine     = require('famous/core/Engine');
-
 var Mesh = require('famous/webgl-renderables/Mesh');
 var Camera = require('famous/components/Camera');
 var Color = require('famous/utilities/Color');
 var DOMElement = require('famous/dom-renderables/DOMElement');
 var math = require('famous/math');
 var physics = require('famous/physics');
-// var PhysicsEngine = require('famous/physics/PhysicsEngine');
-// var Force = PhysicsEngine.Force;
-// var DeviceView = require('./DeviceView');
+var Transform = require('famous/core/Transform');
+var Gravity1D = physics.Gravity1D;
+var Gravity3D = physics.Gravity3D;
+var Particle = physics.Particle;
+var Sphere = physics.Sphere;
+var FamousEngine = require('famous/core/FamousEngine');
 
 function GameApp(scene, num) {
-  var Gravity1D = physics.Gravity1D;
-  var Gravity3D = physics.Gravity3D;
-  var Particle = physics.Particle;
-  var Sphere = physics.Sphere;
   var world = new physics.PhysicsEngine();
   var camera = new Camera(scene).setDepth(1000);
 
-  var FamousEngine = require('famous/core/FamousEngine');
-  // var mainContext = FamousEngine.createContext();
+  var context = FamousEngine.getContext('body');
 
   var numAsteroids = num || 5;
   var asteroidNodes = [];
@@ -52,66 +47,50 @@ function GameApp(scene, num) {
     asteroidNodes.push(asteroidNode);
   }
 
-  var shipSphere = new Sphere({ mass: 50, radius: 50 }).setVelocity([1,1,0]);
-  // var asteroidSphere = new Sphere({ mass: 100, radius: 25 });
+  var shipSphere = new Sphere({ mass: 50, radius: 50, position: shipNode.position });
+  world.add(shipSphere);
 
-
-  // var gameCanvas = new Mesh(gameNode).setGeometry('Box');
+  shipSphere.setVelocity([0.1, 0.1, 0]);
 
   var ship = new Mesh(shipNode, shipSphere).setGeometry('Sphere', { detail: 100 });
   for (var i = 0; i < numAsteroids; i++) {
-    var asteroidSphere = new Sphere({ mass: 100, radius: 25 }).setForce(1,1,1).setVelocity(1,1,1);
-    asteroidBodies.push(asteroidSphere);
+    var asteroidSphere = new Sphere({
+      mass: 100,
+      radius: 25,
+      position: asteroidNodes[i].position,
+      velocity: [0.5, 0.5, 0.5]
+    });
     var asteroid = new Mesh(asteroidNodes[i], asteroidSphere).setGeometry('Sphere');
+    world.add(asteroidSphere);
+    asteroidBodies.push(asteroidSphere);
     asteroidViews.push(asteroid);
-    // var asteroid = new Mesh(asteroidNodes[i]).setGeometry('Sphere');
   }
 
-  for (var i = 0; i < asteroidBodies.length; i++) {
-    var gravity = new Gravity3D(asteroidBodies[i], asteroidBodies, {strength: 300});
-    gravity.init();
-    world.addForce(gravity);
+  for (var i = 0; i < asteroidBodies.length-1; i++) {
+    var gravity = new Gravity1D(asteroidBodies[i], [shipSphere], {strength: 300});
+    world.add(gravity);
   }
-
-
-  world.add([shipSphere].concat(asteroidBodies));
-
-  var Collision = physics.Collision;
-  var collision = new Collision([shipSphere].concat(asteroidBodies));
-  world.addConstraint(collision);
 
   var lightNode = scene.addChild().setAlign(0.5, 0.5, 0.5).setPosition(0, 0, 250);
-
   var light = new PointLight(lightNode).setColor(new Color('white'));
-
   var clock = FamousEngine.getClock();
-
-  // var initialPush = new Force(asteroidNodes);
-
-  // for (var i = 0; i < numAsteroids; i++) {
-  //   initialPush.add(asteroidNodes[i]);
-  // }
-
-  // mainContext.add(asteroidViews[0]);
 
   FamousEngine.getClock().setInterval(function() {
     var time = clock.getTime();
     world.update(time);
-
     shipNode.setRotation(
       time / 1500,
       time / 1200,
       time / 1300
     );
 
-    for (var j = 0; j < numAsteroids; j++) {
-      asteroidNodes[j].setRotation(
-        time / 300,
-        time / 300,
-        time / 300
-      );
-      // asteroidBodies[j].update();
-    }
+    // for (var j = 0; j < numAsteroids; j++) {
+    //   asteroidNodes[j].setRotation(
+    //     time / 300,
+    //     time / 300,
+    //     time / 300
+    //   );
+    // }
     // FamousEngine.update();
   }, 16);
 }
